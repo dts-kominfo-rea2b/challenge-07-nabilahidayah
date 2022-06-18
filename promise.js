d6dfae42dbd86ed05f761b5230f006868526ecba
nabilahdyh <nabilahidayah.nh@gmail.com>
2022-06-18 08:21:28 +0700

    Chall 07

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (sentiment) => {
+  try {
+    const [resIXX, resVGC] = await Promise.all([
+      promiseTheaterIXX(),
+      promiseTheaterVGC(),
+    ]);
+    // combine the rest of responses
+    const result = [...resIXX, ...resVGC];
+
+    // filter result
+    return result?.filter((item) => item.hasil === sentiment)?.length || 0;
+  } catch (error) {
+    console.log("there was an error: " + error);
+  }
+};
 
 module.exports = {
   promiseOutput,
